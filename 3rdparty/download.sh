9d7d4eb5a47c44707bbbcdaa0f2fb7c8d1f86122
Jérémie Dumas <jeremie.dumas@ens-lyon.org>
2017-08-09 15:16:55 -0400

    Build geogram along with the voxmesh binary.

---
+++ 3rdparty/download.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+# Get geogram
+wget https://gforge.inria.fr/frs/download.php/file/36913/geogram_1.4.9.tar.gz
+tar -xvf geogram_1.4.9.tar.gz
+mv geogram_1.4.9 geogram
+rm geogram_1.4.9.tar.gz
